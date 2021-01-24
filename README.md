4d07ec81af6d744b0c430555d37b0ad22e639cc5
Roan Vickerman <29734170+14ROVI@users.noreply.github.com>
2021-01-24 00:57:04 +0000

    added doc strings

--- README.md
+++ README.md
@@ -16,31 +16,31 @@ async fn main() {
     c = topgg::Topgg::new(bot_id, topgg_token);
     
     // gets the top.gg info about your bot
-    c.bot(None);
+    c.bot(None).await;
     
     // gets the top.gg info about another bot
-    c.bot(Some(another_bot_id));
+    c.bot(Some(another_bot_id)).await;
     
     // gets info about a user
-    c.user(Some(a_user_id));
+    c.user(Some(a_user_id)).await;
     
     // gets the IDs of the people who have voted for your bot (the id you initialised with)
-    c.votes();
+    c.votes().await;
     
     // checks if a user has voted for the bot you initalised wiht
-    c.voted(user_id);
+    c.voted(user_id).await;
     
     // gets stats about the server count, servers per shard, and shard count
-    c.get_bot_stats(Some(another_bot_id));
-    c.get_bot_stats(None); // or your bot
+    c.get_bot_stats(Some(another_bot_id)).await;
+    c.get_bot_stats(None).await; // or your bot
     
     // simply posts the server count to top.gg 
-    c.post_bot_stats(Some(server_count), None, None, None);
+    c.post_bot_stats(Some(server_count), None, None, None).await;
     
     // It can also post more complex data like the servers per shard, shard_id of the server count, and shard count
-    c.post_bot_stats(None, Some(shards), None, None);
-    c.post_bot_stats(Some(server_count), None, Some(shard_id_that_posted), None);
-    c.post_bot_stats(Some(server_count), None, None, Some(shard_count));
+    c.post_bot_stats(None, Some(shards), None, None).await;
+    c.post_bot_stats(Some(server_count), None, Some(shard_id_that_posted), None).await;
+    c.post_bot_stats(Some(server_count), None, None, Some(shard_count)).await;
 }
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,26 @@ use serde::{Deserialize, Serialize};
 const BASE_URL: &str = "https://top.gg/api";
 
 
+/// This is the top.gg API client. It houses the functions needed to interact with their API.
 pub struct Topgg {
     bot_id: u64,
     token: String,
     client: reqwest::Client
 }
 impl Topgg {
+    /// Returns a new client.
+    /// 
+    /// ## Arguments
+    /// * `bot_id` - The ID of your bot
+    /// * `token` - The top.gg token for that (or another valid) bot
+    /// 
+    /// ## Examples
+    /// ```
+    /// let client = topgg::Topgg::new(bot_id, token);
+    /// // Do stuff with the client
+    /// let votes = client.votes().await;
+    /// ```
+    /// 
     pub fn new(bot_id: u64, token: String) -> Topgg {
         Topgg {
             bot_id: bot_id,
@@ -20,6 +34,20 @@ impl Topgg {
     }
 
 
+    /// A shortcut for getting the botinfo for your own bot.
+    pub async fn my_bot(&self) -> Bot {
+        self.bot(None).await
+    }
+
+
+    /// Gets the info for a bot given an ID. If no ID is specified then it will use the one you used when setting up the client.
+    /// ## Examples
+    /// ```
+    /// // Gets the info for the client's bot_id
+    /// client.bot(None).await;
+    /// // Gets info for a different bot
+    /// client.bot(Some(668701133069352961)).await;
+    /// ```
     pub async fn bot(&self, bot_id: Option<u64>) -> Bot {
         let bot_id = bot_id.unwrap_or(self.bot_id);
         let url = format!("{}/bots/{}", BASE_URL, bot_id);
@@ -60,6 +88,11 @@ impl Topgg {
     }
 
 
+    /// Gets the info for a user.
+    /// ## Examples
+    /// ```
+    /// client.user(195512978634833920).await;
+    /// ```
     pub async fn user(&self, user_id: u64) -> User {
         let url = format!("{}/users/{}", BASE_URL, user_id);
         let res = self.client
@@ -95,6 +128,11 @@ impl Topgg {
     }
 
 
+    /// Gets the user IDs of all the users that have voted on the bot_id used when creating the client.
+    /// ## Examples
+    /// ```
+    /// client.votes().await;
+    /// ```
     pub async fn votes(&self) -> Vec<u64> {
         let url = format!("{}/bots/{}/votes", BASE_URL, self.bot_id);
         let res = self.client
@@ -111,6 +149,11 @@ impl Topgg {
     }
 
 
+    /// Checks if a user has voted for the bot or not. Returns true if they have, false if they have not.alloc
+    /// ## Examples
+    /// ```
+    /// let voted = client.voted(195512978634833920).await;
+    /// ```
     pub async fn voted(&self, user_id: u64) -> bool {
         let url = format!("{}/bots/{}/check?userId={}", BASE_URL, self.bot_id, user_id);
         let res = self.client
@@ -131,6 +174,14 @@ impl Topgg {
     }
 
 
+    /// Gets the 'stats' of the bot, this includes the server count, shard count, and shards (servers per shard).
+    /// ## Examples
+    /// ```
+    /// // Gets the stats for your own bot
+    /// client.get_bot_stats(None).await
+    /// // Gets the stats for another bot
+    /// client.get_bot_stats(Some(668701133069352961)).await
+    /// ```
     pub async fn get_bot_stats(&self, bot_id: Option<u64>) -> BotStats {
         let bot_id = bot_id.unwrap_or(self.bot_id);
         let url = format!("{}/bots/{}/stats", BASE_URL, bot_id);
@@ -148,6 +199,13 @@ impl Topgg {
     }
 
     
+    /// This posts the stats for your bot. Useful if you want to update the server count on your top.gg bot page. You can omit from having a `server_count` if you use `shards` where it is a Vec of the number of servers per shard. `shard_id` is only applicable if you use `sever_count` and it tells top.gg the number of servers for that indexed shard.
+    /// ## Examples
+    /// ```
+    /// client.post_bot_stats(None, Some(vec![142, 532, 304]), None, None).await;
+    /// client.post_bot_stats(Some(142), None, Some(0), None).await;
+    /// client.post_bot_stats(Some(978), None, None, Some(3)).await;
+    /// ```
     pub async fn post_bot_stats(&self, server_count: Option<u32>, shards: Option<Vec<u32>>, shard_id: Option<u32>, shard_count: Option<u32>) {
         if server_count.is_none() && shards.is_none() {
             return;
